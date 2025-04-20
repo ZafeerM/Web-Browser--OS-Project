cee8a3830998f5b2dc5d1bf57e4659a8606f908e
ZafeerM <93424492+ZafeerM@users.noreply.github.com>
2025-04-20 17:54:50 +0500

    Update Week #02 - Sockets Connection, HTTPS handling.c

--- Week #02 - Sockets Connection, HTTPS handling.c
+++ Week #02 - Sockets Connection, HTTPS handling.c
@@ -6,61 +6,56 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-int main()
+//---------Zafeer's Functions (HTTP and Sockets)----------
+char* URLtoIPconverter(char url[256])
 {
-    
-    // 1- URL input (can be from terminal too)
-    char url[256];
-    printf("Enter a string: ");
-    fgets(url, sizeof(url), stdin);
-    url[strcspn(url, "\n")] = 0; 
-    
-
-    
     struct hostent *host = gethostbyname(url);
     if (host == NULL) 
     {
         fprintf(stderr, "ERROR : Host Name Not Found.\n");
-        return 1;
+        return "error";
     }
 
-    // Convert to IP string
-    char *ip = inet_ntoa(*((struct in_addr *)host->h_addr_list[0]));
+    return inet_ntoa(*((struct in_addr *)host->h_addr_list[0]));
+}
 
-    printf("IP address of %s is: %s\n", url, ip);
-
-
-    // 2- Creating socket
+int createSocket()
+{
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
     {
         perror("Socket creation failed");
-        return 1;
+        exit(1);
     }
+    return sockfd;
+}
 
-    // 3- Setup server address
+void connectToServer(int sockfd, char* ip)
+{
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(80);  // I connected on port 80
+    server_addr.sin_port = htons(80);
     inet_pton(AF_INET, ip, &server_addr.sin_addr);
 
-    // 4- Connect to server
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) 
     {
         perror("Connection failed");
-        return 1;
+        exit(1);
     }
 
     printf("Connected to %s on port 80\n", ip);
+}
 
-
-    // 5- Send HTTP request
+void sendHTTPRequest(int sockfd, char* url)
+{
     char request[1024];
     sprintf(request, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", url);
     send(sockfd, request, strlen(request), 0);
     printf("HTTP request sent!\n");
+}
 
-    // 6- Receive and print the response
+void saveResponseToFile(int sockfd)
+{
     char buffer[4096];
     int bytes_received;
     FILE *file = fopen("History/output.html", "w");
@@ -69,20 +64,36 @@ int main()
     {
         perror("File opening failed");
         close(sockfd);
-        return 1;
+        exit(1);
     }
 
     while ((bytes_received = recv(sockfd, buffer, sizeof(buffer) - 1, 0)) > 0) 
     {
-        buffer[bytes_received] = '\0'; // Null-terminate
-        fwrite(buffer, 1, bytes_received, file); // Save to file
+        buffer[bytes_received] = '\0';
+        fwrite(buffer, 1, bytes_received, file);
     }
 
+    fclose(file);
+    printf("\n\nResponse received. File saved.\n");
+}
 
-    printf("\n\nResponse received. Closing socket.\n");
-    
+int main()
+{
+    char url[256];
+    printf("Enter a string: ");
+    fgets(url, sizeof(url), stdin);
+    url[strcspn(url, "\n")] = 0;
 
+    char* ip = URLtoIPconverter(url);
+    if (strcmp(ip, "error") == 0)
+        return 1;
 
+    printf("IP address of %s is: %s\n", url, ip);
+
+    int sockfd = createSocket();
+    connectToServer(sockfd, ip);
+    sendHTTPRequest(sockfd, url);
+    saveResponseToFile(sockfd);
     close(sockfd);
 
     return 0;
